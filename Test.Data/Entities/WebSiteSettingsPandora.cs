d8fbfc4022fc81aecba4f99ae1b3fceeae60656d
ValeriiR <valeriirohachko@gmail.com>
2019-02-18 12:51:48 +0200

    GetSalaryFromDescription added

--- Test.Data/Entities/WebSiteSettingsPandora.cs
+++ Test.Data/Entities/WebSiteSettingsPandora.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using HtmlAgilityPack;
 using Test.Data.Repositories.Abstract;
 
@@ -9,23 +10,27 @@ namespace Test.Data.Entities
 {
     public class WebSiteSettingsPandora : IWebSiteSettings
     {
-        public  string Domain { get; set; }
+        public string Domain { get; set; }
 
-        private List<string> Domains = new List<string>();
+        private List<DomainsToParse> domainsToParse = new List<DomainsToParse>();
 
         public WebSiteSettingsPandora()
         {
             Domain = "pandorajob.ru";
 
-            Domains.Add("http://pandorajob.ru/office");
+            domainsToParse.Add(new DomainsToParse("http://pandorajob.ru/office"));
 
         }
 
         public List<Vacancy> Parse(string urlss)
         {
+            //  GetDescriptionFromVacancyLink("http://pandorajob.ru/ekaterinburg");
+
+
+
             List<Vacancy> vacancies = new List<Vacancy>();
 
-            var urls = ParsePandoraRegions();
+            List<string> urls = ParsePandoraRegions();
 
             HtmlWeb web = new HtmlWeb();
 
@@ -50,15 +55,17 @@ namespace Test.Data.Entities
                     {
                         try
                         {
-                            //Regex regex = new Regex(@"туп(\w*)");
-                            string text = nodeSalaryInfo[i].InnerText;
 
-                            string salary = text.Split(new string[] { "Зарплата от ", " р." },
-                                System.StringSplitOptions.RemoveEmptyEntries).Single();
+                            string desc = nodeSalaryInfo[i].InnerText;
+
+                            string salary = GetSalaryFromDescription(desc);
+
+                            //string salary = text.Split(new string[] { "Зарплата от ", " р." },
+                            //    System.StringSplitOptions.RemoveEmptyEntries).Single();
 
 
-                            text = nodeVacancyInfo[i].InnerText;
-
+                            string text = nodeVacancyInfo[i].InnerText;
+                           
                             string city = text.Split(new char[] { '.' },
                                     System.StringSplitOptions.RemoveEmptyEntries)
                                 .FirstOrDefault();
@@ -94,13 +101,54 @@ namespace Test.Data.Entities
             return vacancies;
         }
 
+        private List<string> GetInfoFromMainBlock(string text)
+        {
+            Regex regex = new Regex(@"(^(\w+)(\.))");
+
+            var match = regex.Match(text);
+            string str = match.Value;
+            string cit = str.TrimEnd('.');
+
+
+
+            regex = new Regex(@"((\. )(\w+)-(\w+))|((\. )(\w+))");
+            match = regex.Match(text);
+            str = match.Value;
+            string vac = str.TrimStart('.');
+
+
+
+            regex = new Regex(@"((\. )(\w+)-(\w+))|((\. )(\w+))");
+
+            if (vac != "")
+            {
+                text = text.Substring(vac.Length + 1);
+
+                string description = text;
+            }
+
+            return new List<string>();
+        }
+
+        private string GetSalaryFromDescription(string text)
+        {
+            Regex regex = new Regex(@" ((\d+)\s(\d+) (р\.))|((\d+)\s(\d+) (руб))");
+
+            var match = regex.Match(text);
+            string str = match.Value;
+            str = str.TrimEnd('.');
+            return str;
+        }
 
         private List<string> ParsePandoraRegions()
         {
             HtmlWeb web = new HtmlWeb();
-            var htmlDoc = web.Load("http://pandorajob.ru/regions");
+            HtmlDocument htmlDoc = web.Load("http://pandorajob.ru/regions");
 
-            var citiesDiv = htmlDoc.DocumentNode.SelectNodes("//div[@class='t650']//a");
+
+            //*[@id="rec23655290"]
+
+            HtmlNodeCollection citiesDiv = htmlDoc.DocumentNode.SelectNodes("//div[@class='t650']//a");
 
 
             List<string> urls = new List<string>();
@@ -112,5 +160,64 @@ namespace Test.Data.Entities
             }
             return urls;
         }
+        private string GetDescriptionFromVacancyLink(string url)
+        {
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument htmlDoc = web.Load(url);
+
+            //  HtmlNodeCollection nodes = htmlDoc.DocumentNode.SelectNodes("//div[@class='t650']//a");
+            HtmlNodeCollection linksElements = htmlDoc.DocumentNode.SelectNodes("//div[@class='t125']//a");
+
+            List<string> links = new List<string>();
+            foreach (var div in linksElements)
+            {
+                var str = div.GetAttributeValue("href", "");
+                string domain = "http://pandorajob.ru";
+                domain = domain + str;
+                links.Add(domain);
+
+            }
+
+
+
+
+            List<string> descs = new List<string>();
+
+            foreach (var link in links)
+            {
+                htmlDoc = web.Load(link);
+
+                HtmlNode description = htmlDoc.DocumentNode.SelectSingleNode("//div[@class='t650']//a");
+
+                descs.Add(description.InnerText);
+            }
+
+
+
+
+
+            //string description = nodes.InnerText;
+
+
+
+            return "";
+        }
+
+
+        private class DomainsToParse
+        {
+            public string Domain { get; set; }
+
+            public bool isParsed { get; set; }
+
+            public DomainsToParse(string domain)
+            {
+                Domain = domain;
+                isParsed = false;
+            }
+        }
     }
+
+
+
 }
